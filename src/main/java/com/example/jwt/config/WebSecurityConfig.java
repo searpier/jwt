6d9d56f1fc1dd618ff27ce51fb85c0acb9c03031
searpier <searpier@naver.com>
2023-01-31 21:35:21 +0900

    initial commit

--- src/main/java/com/example/jwt/config/WebSecurityConfig.java
+++ src/main/java/com/example/jwt/config/WebSecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
+
 @RequiredArgsConstructor
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
